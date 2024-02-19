87fc85287616019d389294d8e6e39bcf6483f127
Tom Payne <twpayne@gmail.com>
2024-02-19 18:50:50 +0100

    chore: tidy up comments

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ func run() error {
 		roots = pflag.Args()
 	}
 
+	// Create a trace file, if requested.
 	if *traceFile != "" {
 		traceFile, err := os.Create(*traceFile)
 		if err != nil {
@@ -39,18 +40,18 @@ func run() error {
 		defer trace.Stop()
 	}
 
+	// Find duplicates.
 	dupFinder := &find.Finder{
 		Roots:              roots,
 		DuplicateThreshold: *threshold,
 		KeepGoing:          *keepGoing,
 	}
-
 	result, err := dupFinder.FindDuplicates()
 	if err != nil {
 		return err
 	}
 
-	// Write JSON outputFile.
+	// Write output file.
 	var outputFile *os.File
 	if *output == "" || *output == "-" {
 		outputFile = os.Stdout
