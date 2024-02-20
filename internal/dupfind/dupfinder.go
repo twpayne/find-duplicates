8aae92ee23f6e0aabc95f83aaba9d567afa8d0d7
Tom Payne <twpayne@gmail.com>
2024-02-20 21:37:47 +0100

    chore: internalize statistics

--- internal/dupfind/dupfinder.go
+++ internal/dupfind/dupfinder.go
@@ -9,21 +9,49 @@ import (
 	"path/filepath"
 	"slices"
 	"sync"
+	"sync/atomic"
 
 	"github.com/panjf2000/ants/v2"
 	"github.com/zeebo/xxh3"
 )
+
+// minCacheLineSize is the minimum cache line size, used to prevent false
+// sharing. Smaller values have an insignificant effect on memory usage. Larger
+// values help separate values into separate cache lines.
+const minCacheLineSize = 128
 
 type DupFinder struct {
 	channelBufferCapacity int
 	errorHandler          func(error) error
 	roots                 []string
 	threshold             int
-	statistics            Statistics
+	statistics            struct {
+		errors      atomic.Uint64
+		_           [minCacheLineSize - 8]byte
+		dirEntries  atomic.Uint64
+		_           [minCacheLineSize - 8]byte
+		totalBytes  atomic.Uint64
+		_           [minCacheLineSize - 8]byte
+		filesOpened atomic.Uint64
+		_           [minCacheLineSize - 8]byte
+		bytesHashed atomic.Uint64
+		_           [minCacheLineSize - 8]byte
+	}
 }
 
 // An Option sets an option on a [*DupFinder].
 type Option func(*DupFinder)
+
+// Statistics contains various statistics.
+type Statistics struct {
+	Errors             uint64  `json:"errors"`
+	DirEntries         uint64  `json:"dirEntries"`
+	FilesOpened        uint64  `json:"filesOpened"`
+	FilesOpenedPercent float64 `json:"filesOpenedPercent"`
+	TotalBytes         uint64  `json:"totalBytes"`
+	BytesHashed        uint64  `json:"bytesHashed"`
+	BytesHashedPercent float64 `json:"bytesHashedPercent"`
+}
 
 // A pathWithSize contains a path to a regular file and its size.
 type pathWithSize struct {
@@ -165,7 +193,21 @@ func (f *DupFinder) FindDuplicates() (map[string][]string, error) {
 }
 
 func (f *DupFinder) Statistics() *Statistics {
-	return &f.statistics
+	errors := f.statistics.errors.Load()
+	dirEntries := f.statistics.dirEntries.Load()
+	filesOpened := f.statistics.filesOpened.Load()
+	totalBytes := f.statistics.totalBytes.Load()
+	bytesHashed := f.statistics.bytesHashed.Load()
+
+	return &Statistics{
+		Errors:             errors,
+		DirEntries:         dirEntries,
+		FilesOpened:        filesOpened,
+		FilesOpenedPercent: 100 * float64(filesOpened) / max(1, float64(dirEntries)),
+		TotalBytes:         totalBytes,
+		BytesHashed:        bytesHashed,
+		BytesHashedPercent: 100 * float64(bytesHashed) / max(1, float64(totalBytes)),
+	}
 }
 
 // concurrentWalkDir is like [fs.WalkDir] except that directories are walked concurrently.
